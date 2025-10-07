12f94f1c5c50427521faff1f52984ea8f195920e
bhaskkkarrr <bhaskarchauhan748@gmail.com>
2025-10-07 22:37:56 +0530

    add project files

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "cms-demo-murex.vercel.app",
     credentials: true,
   })
 );
